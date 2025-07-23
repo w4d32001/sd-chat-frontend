cc8235cfc8e048dea0498e7037c347394b6f1ad7
w4d3 <morph@r2d2.r2d2.com>
2025-07-23 10:46:02 -0500

    vercel-connection2

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://sd-chat-backend.onrender.com" : "https://sd-chat-backend.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://sd-chat-backend.onrender.com/api" : "https://sd-chat-backend.onrender.com/api",
   withCredentials: true,
 });
