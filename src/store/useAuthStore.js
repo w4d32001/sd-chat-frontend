b523aefc89250f74aaa58655feb1debf7b7ad078
w4d3 <morph@r2d2.r2d2.com>
2025-07-23 10:59:27 -0500

    render-connection

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://sd-chat-backend.onrender.com" : "https://sd-chat-backend.onrender.com/";
+const BASE_URL = "https://sd-chat-backend.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
