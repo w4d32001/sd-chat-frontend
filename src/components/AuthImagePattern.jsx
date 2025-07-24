3d954d9d55921d34ed0d4c5a653eec0aea67211b
w4d3 <morph@r2d2.r2d2.com>
2025-07-23 21:50:50 -0500

    login

--- src/components/AuthImagePattern.jsx
+++ src/components/AuthImagePattern.jsx
@@ -1,21 +1,9 @@
 const AuthImagePattern = ({ title, subtitle }) => {
     return (
-        <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
+        <div className="hidden lg:flex items-center justify-center bg-base-200 p-12 rounded-r-3xl">
             <div className="max-w-md text-center">
-                <div className="grid grid-cols-3 gap-3 mb-8">
-                    {[...Array(9)].map((_, i) => (
-                        <div
-                            key={i}
-                            className={`aspect-square rounded-2xl bg-primary/10 flex items-center justify-center text-3xl font-bold ${
-                                i % 2 === 0 ? "animate-pulse" : ""
-                            }`}
-                        >
-                           {
-                            i % 2 === 0 ? "X" : "O"
-                                
-                           }
-                        </div>
-                    ))}
+                <div className=" gap-3 mb-8 w-full">
+                    <img src="/login2.png" alt="" className="w-[500px] h-[500px]" />
                 </div>
                 <h2 className="text-2xl font-bold mb-4">{title}</h2>
                 <p className="text-base-content/60">{subtitle}</p>
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,121 +5,144 @@ import { Link } from "react-router-dom";
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare } from "lucide-react";
 
 const LoginPage = () => {
-  const [showPassword, setShowPassword] = useState(false);
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-  const { login, isLoggingIn } = useAuthStore();
+    const [showPassword, setShowPassword] = useState(false);
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+    const { login, isLoggingIn } = useAuthStore();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    login(formData);
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        login(formData);
+    };
 
-  return (
-    <div className="h-screen grid lg:grid-cols-2">
-      <div className="flex flex-col justify-center items-center p-6 sm:p-12">
-        <div className="w-full max-w-md space-y-8">
-          <div className="text-center mb-8">
-            <div className="flex flex-col items-center gap-2 group">
-              <div
-                className="w-12 h-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20
+    return (
+        <div className="h-screen p-32">
+            <div className="grid lg:grid-cols-2 border rounded-3xl shadow-2xl">
+                <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+                    <div className="w-full max-w-md space-y-8">
+                        <div className="text-center mb-8">
+                            <div className="flex flex-col items-center gap-2 group">
+                                <div
+                                    className="w-12 h-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20
               transition-colors"
-              >
-                <MessageSquare className="w-6 h-6 text-primary" />
-              </div>
-              <h1 className="text-2xl font-bold mt-2">Bienvenido de nuevo</h1>
-              <p className="text-base-content/60">Inicia sesión en tu cuenta</p>
-            </div>
-          </div>
+                                >
+                                    <MessageSquare className="w-6 h-6 text-primary" />
+                                </div>
+                                <h1 className="text-2xl font-bold mt-2">
+                                    Bienvenido de nuevo
+                                </h1>
+                                <p className="text-base-content/60">
+                                    Inicia sesión en tu cuenta
+                                </p>
+                            </div>
+                        </div>
 
-          <form onSubmit={handleSubmit} className="space-y-6">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text font-medium">Correo</span>
-              </label>
-              <div className="relative">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <Mail className="h-5 w-5 text-base-content/40" />
+                        <form onSubmit={handleSubmit} className="space-y-6">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-medium">
+                                        Correo
+                                    </span>
+                                </label>
+                                <div className="relative">
+                                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                        <Mail className="h-5 w-5 text-base-content/40" />
+                                    </div>
+                                    <input
+                                        type="email"
+                                        className={`input input-bordered w-full pl-10`}
+                                        placeholder="correo@example.com"
+                                        value={formData.email}
+                                        onChange={(e) =>
+                                            setFormData({
+                                                ...formData,
+                                                email: e.target.value,
+                                            })
+                                        }
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-medium">
+                                        Contraseña
+                                    </span>
+                                </label>
+                                <div className="relative">
+                                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                        <Lock className="h-5 w-5 text-base-content/40" />
+                                    </div>
+                                    <input
+                                        type={
+                                            showPassword ? "text" : "password"
+                                        }
+                                        className={`input input-bordered w-full pl-10`}
+                                        placeholder="••••••••"
+                                        value={formData.password}
+                                        onChange={(e) =>
+                                            setFormData({
+                                                ...formData,
+                                                password: e.target.value,
+                                            })
+                                        }
+                                    />
+                                    <button
+                                        type="button"
+                                        className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                                        onClick={() =>
+                                            setShowPassword(!showPassword)
+                                        }
+                                    >
+                                        {showPassword ? (
+                                            <EyeOff className="h-5 w-5 text-base-content/40" />
+                                        ) : (
+                                            <Eye className="h-5 w-5 text-base-content/40" />
+                                        )}
+                                    </button>
+                                </div>
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="btn btn-primary w-full"
+                                disabled={isLoggingIn}
+                            >
+                                {isLoggingIn ? (
+                                    <>
+                                        <Loader2 className="h-5 w-5 animate-spin" />
+                                        Cargando...
+                                    </>
+                                ) : (
+                                    "Iniciar sesión"
+                                )}
+                            </button>
+                        </form>
+
+                        <div className="text-center">
+                            <p className="text-base-content/60">
+                                No tienes una cuenta?{" "}
+                                <Link
+                                    to="/signup"
+                                    className="link link-primary"
+                                >
+                                    Crear cuenta
+                                </Link>
+                            </p>
+                        </div>
+                    </div>
                 </div>
-                <input
-                  type="email"
-                  className={`input input-bordered w-full pl-10`}
-                  placeholder="correo@example.com"
-                  value={formData.email}
-                  onChange={(e) =>
-                    setFormData({ ...formData, email: e.target.value })
-                  }
+
+                <AuthImagePattern
+                    title={"Bienvenido de nuevo!"}
+                    subtitle={
+                        "Inicia sesión para continuar tus conversaciones y ponerte al día con tus mensajes."
+                    }
                 />
-              </div>
             </div>
-
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text font-medium">Contraseña</span>
-              </label>
-              <div className="relative">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <Lock className="h-5 w-5 text-base-content/40" />
-                </div>
-                <input
-                  type={showPassword ? "text" : "password"}
-                  className={`input input-bordered w-full pl-10`}
-                  placeholder="••••••••"
-                  value={formData.password}
-                  onChange={(e) =>
-                    setFormData({ ...formData, password: e.target.value })
-                  }
-                />
-                <button
-                  type="button"
-                  className="absolute inset-y-0 right-0 pr-3 flex items-center"
-                  onClick={() => setShowPassword(!showPassword)}
-                >
-                  {showPassword ? (
-                    <EyeOff className="h-5 w-5 text-base-content/40" />
-                  ) : (
-                    <Eye className="h-5 w-5 text-base-content/40" />
-                  )}
-                </button>
-              </div>
-            </div>
-
-            <button
-              type="submit"
-              className="btn btn-primary w-full"
-              disabled={isLoggingIn}
-            >
-              {isLoggingIn ? (
-                <>
-                  <Loader2 className="h-5 w-5 animate-spin" />
-                  Cargando...
-                </>
-              ) : (
-                "Iniciar sesión"
-              )}
-            </button>
-          </form>
-
-          <div className="text-center">
-            <p className="text-base-content/60">
-              No tienes una cuenta?{" "}
-              <Link to="/signup" className="link link-primary">
-                Crear cuenta
-              </Link>
-            </p>
-          </div>
         </div>
-      </div>
-
-      <AuthImagePattern
-        title={"Bienvenido de nuevo!"}
-        subtitle={
-          "Inicia sesión para continuar tus conversaciones y ponerte al día con tus mensajes."
-        }
-      />
-    </div>
-  );
+    );
 };
 export default LoginPage;
